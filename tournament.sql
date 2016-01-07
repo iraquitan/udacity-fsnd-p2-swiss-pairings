397d887bb4679327046f9770bb35278926e92c8c
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-01-07 17:46:37 -0300

    feat: database now supports more than one tournament

--- tournament.sql
+++ tournament.sql
@@ -13,59 +13,77 @@
 -- CREATE DATABASE tournament;
 
 -- Connect to tournament database
--- \c tournament;
+-- \connect tournament;
 
--- Create Players table
+-- Create Player table
 CREATE TABLE players (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(20),
-  last_name VARCHAR(30),
+  name TEXT,
   date_created TIMESTAMP DEFAULT current_timestamp
 );
 
 -- Create tournament table
--- CREATE TABLE tournament (
---   id SERIAL PRIMARY KEY,
---   num_of_players INTEGER,
---   date_created TIMESTAMP DEFAULT current_timestamp
--- );
-
--- Create matches table
-CREATE TABLE matches (
+CREATE TABLE tournaments (
   id SERIAL PRIMARY KEY,
---   tournament_id INTEGER REFERENCES tournament,
-  winner_id INTEGER REFERENCES players,
-  loser_id INTEGER REFERENCES players,
+  num_of_players INTEGER,
   date_created TIMESTAMP DEFAULT current_timestamp
 );
 
--- Create standings view
-CREATE VIEW standings AS
-    SELECT players.id, players.first_name, players.last_name,
-      (SELECT count(matches.id)
-       FROM matches
-       WHERE players.id = matches.winner_id) AS wins,
-      (SELECT count(matches.id)
-       FROM matches
-       WHERE players.id = matches.winner_id
-             OR players.id = matches.loser_id) AS matches_num
-    FROM players LEFT JOIN matches
-         ON matches.id = players.id
---     GROUP BY players.id
-    ORDER BY wins DESC;
+-- Create Tournament Player table
+CREATE TABLE tournament_players (
+  id SERIAL PRIMARY KEY,
+  player_id INTEGER REFERENCES players,
+  tournament_id INTEGER REFERENCES tournaments,
+  date_created TIMESTAMP DEFAULT current_timestamp
+);
 
--- Trigger
+-- Create match table
+CREATE TABLE matches (
+  id SERIAL PRIMARY KEY,
+  tournament_id INTEGER REFERENCES tournaments,
+  winner_id INTEGER REFERENCES tournament_players,
+  loser_id INTEGER REFERENCES tournament_players,
+  date_created TIMESTAMP DEFAULT current_timestamp
+);
+
+-- Create new standings view
+-- TODO Add OMW to sort standings as tiebreaker
+CREATE VIEW standings AS
+  SELECT tournaments.id AS t_id, players.id AS p_id, players.name,
+      (SELECT count(matches.id)
+       FROM matches
+       WHERE tournament_players.player_id = matches.winner_id
+             AND matches.tournament_id = tournament_players.tournament_id
+      ) AS wins,
+      (SELECT count(matches.id)
+       FROM matches
+       WHERE (tournament_players.player_id = matches.winner_id
+             AND matches.tournament_id = tournament_players.tournament_id)
+             OR (tournament_players.player_id = matches.loser_id
+                 AND matches.tournament_id = tournament_players.tournament_id)
+      ) AS matches_played
+    FROM players
+      LEFT JOIN tournament_players
+        ON players.id = tournament_players.player_id
+      LEFT JOIN tournaments
+        ON tournaments.id = tournament_players.tournament_id;
+
+-- Trigger to check if player is participating on tournament
 CREATE FUNCTION check_player() RETURNS trigger AS $$
 DECLARE
   player1 BOOLEAN;
   player2 BOOLEAN;
 BEGIN
-  player1 := (SELECT exists(SELECT * FROM players WHERE id = NEW.winner_id));
-  player2 := (SELECT exists(SELECT * FROM players WHERE id = NEW.loser_id));
+  player1 := (SELECT exists(
+      SELECT * FROM tournament_players
+      WHERE player_id = NEW.winner_id AND tournament_id = NEW.tournament_id));
+  player2 := (SELECT exists(
+      SELECT * FROM tournament_players
+      WHERE player_id = NEW.loser_id AND tournament_id = NEW.tournament_id));
   IF player1 IS FALSE THEN
-    RAISE EXCEPTION 'player1 id not in players TABLE';
+    RAISE EXCEPTION 'player1 id not in tournament_players TABLE';
   ELSIF player2 IS FALSE THEN
-    RAISE EXCEPTION 'player2 id not in players TABLE';
+    RAISE EXCEPTION 'player2 id not in tournament_players TABLE';
   END IF;
   RETURN NEW;
 END;
@@ -76,3 +94,28 @@ CREATE TRIGGER check_player_trg
     ON matches
     FOR EACH ROW
     EXECUTE PROCEDURE check_player();
+
+-- Trigger to check if player is participating on tournament
+CREATE FUNCTION check_tournament() RETURNS trigger AS $$
+DECLARE
+  subscribed_players INTEGER;
+  num_players INTEGER;
+BEGIN
+  subscribed_players := (SELECT count(player_id)
+                         FROM tournament_players
+                         WHERE tournament_id = NEW.tournament_id);
+  num_players = (SELECT num_of_players
+                 FROM tournaments
+                 WHERE tournaments.id = NEW.tournament_id);
+  IF subscribed_players >= num_players THEN
+    RAISE EXCEPTION 'tournament already full!';
+  END IF;
+  RETURN NEW;
+END;
+$$ language plpgsql;
+
+CREATE TRIGGER check_tournament_trg
+    BEFORE INSERT OR UPDATE
+    ON tournament_players
+    FOR EACH ROW
+    EXECUTE PROCEDURE check_tournament();
