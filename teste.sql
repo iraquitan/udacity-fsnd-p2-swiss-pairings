691673448c37c7915ff5d700d70222eacd1fe4e6
Iraquitan Cordeiro Filho <iraquitanfilho@gmail.com>
2016-01-08 17:46:14 -0300

    feat: Add new SQL tests

--- teste.sql
+++ teste.sql
@@ -26,28 +26,32 @@ INSERT INTO matches (winner_id, loser_id) VALUES (1, 4);
 -- INSERT INTO matches (winner_id, loser_id) VALUES (2, 1);
 -- INSERT INTO matches (winner_id, loser_id) VALUES (3, 4);
 
+INSERT INTO matches (tournament_id, winner_id, loser_id) VALUES (5, 33, 35);
+
 -- SELECT * from standings;
 
 -- SELECT count(id) FROM players;
 
-SELECT a.id AS a_id, b.id AS b_id, a.wins
-from standings AS a JOIN standings AS b
-        ON a.id < b.id
-WHERE a.wins = b.wins;
+SELECT a.p_id AS a_id, b.p_id AS b_id, b.name AS b_name, a.wins
+FROM standings AS a LEFT JOIN standings AS b
+        ON a.p_id <> b.p_id AND a.t_id = b.t_id
+WHERE a.wins = b.wins AND a.p_id = 41 AND a.t_id = 7;
 
 
-SELECT * FROM (SELECT a.id AS a_id, b.id AS b_id, a.wins
-               from standings AS a
-                 LEFT JOIN standings AS b ON a.id < b.id
+SELECT * FROM (SELECT a.p_id AS a_id, b.p_id AS b_id, a.wins
+               FROM standings AS a
+                 LEFT JOIN standings AS b ON a.p_id < b.p_id
                WHERE a.wins = b.wins) AS swiss;
 
 -- OMW (Opponent Match Wins)
-SELECT a.id AS a_id, sum(o.wins) AS OMW
+SELECT a.t_id, a.p_id AS a_id, sum(o.wins) AS OMW
 FROM standings AS a
-  JOIN matches ON a.id = matches.winner_id OR a.id = matches.loser_id
-  JOIN standings AS o
-    ON a.id <> o.id AND (o.id = matches.winner_id OR o.id = matches.loser_id)
-GROUP BY a.id;
+  LEFT JOIN matches ON (
+    a.p_id = matches.winner_id OR a.p_id = matches.loser_id) AND a.t_id = matches.tournament_id
+  LEFT JOIN standings AS o
+    ON a.p_id <> o.p_id AND (o.p_id = matches.winner_id OR o.p_id = matches.loser_id)
+  WHERE a.t_id = matches.tournament_id AND a.t_id = o.t_id
+GROUP BY a.p_id, a.t_id;
 
 -- OMP (Opponent Match Percentage)
 SELECT a.id AS a_id, sum(o.wins::FLOAT/(o.matches_num))/count(o.wins) AS OMP
@@ -61,3 +65,16 @@ GROUP BY a.id;
 SELECT exists(SELECT * FROM players WHERE id = 97);
 
 -- DROP TRIGGER check_player_trg ON matches;
+
+SELECT * FROM standings JOIN
+  (SELECT a.t_id, a.p_id AS a_id, sum(o.wins) AS OMW
+FROM standings AS a
+  LEFT JOIN matches ON (
+    a.p_id = matches.winner_id OR a.p_id = matches.loser_id) AND a.t_id = matches.tournament_id
+  LEFT JOIN standings AS o
+    ON a.p_id <> o.p_id AND (o.p_id = matches.winner_id OR o.p_id = matches.loser_id)
+  WHERE a.t_id = matches.tournament_id AND a.t_id = o.t_id
+GROUP BY a.p_id, a.t_id) as OMW2 ON OMW2.t_id = standings.t_id AND OMW2.a_id = standings.p_id;
+
+SELECT *
+FROM standings_owm WHERE t_id = 21 ORDER BY wins DESC, omw DESC ;
